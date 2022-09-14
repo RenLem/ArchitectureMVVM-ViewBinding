6907ae18c050353b243144b7495f2463f09bec08
From West <ag.tropico@gmail.com>
2022-09-14 15:18:07 +0200

    Architecture view model with view binding

--- app/src/main/java/by/renatopuskaric/architecturemvvm/MainActivity.kt
+++ app/src/main/java/by/renatopuskaric/architecturemvvm/MainActivity.kt
@@ -2,6 +2,7 @@ package by.renatopuskaric.architecturemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.LifecycleOwner
@@ -9,18 +10,20 @@ import androidx.lifecycle.ViewModelProvider
 import by.renatopuskaric.architecturemvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView: TextView = findViewById(R.id.textView)
-        val button: Button = findViewById(R.id.button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val  view: View = binding.root
+        setContentView(view)
 
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             var textToDieplay = viewModel.upAdd().toString()
-            textView.text = textToDieplay
+            binding.textView.text = textToDieplay
         }
 
 
